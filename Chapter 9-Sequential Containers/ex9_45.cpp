7279b07ce369aa2f4fd4dfd3441be213e1c9b3e3
Liang Hong <hoilus@users.noreply.github.com>
2018-05-20 11:12:52 -0500

    Create ex9_45.cpp

---
+++ Chapter 9-Sequential Containers/ex9_45.cpp
@@ -0,0 +1,27 @@
+/*
+Exercise 9.45: Write a funtion that takes a string representing a name
+and two other strings representing a prefix, such as “Mr.” or “Ms.” and a
+suffix, such as “Jr.” or “III”. Using iterators and the insert and append
+functions, generate and return a new string with the suffix and prefix
+added to the given name.
+*/
+
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <cstddef>
+
+using namespace std;
+
+string full_name(string& s, string const& prefix, string const& suffix) {
+    s.insert(s.begin(), prefix.cbegin(), prefix.cend());
+    return s.append(suffix);
+}
+
+int main()
+{
+    string name = {"Jessica"};
+    cout << full_name(name, "Ms.", "I") << endl;
+    
+    return 1;
+}
