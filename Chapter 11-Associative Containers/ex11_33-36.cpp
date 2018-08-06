afc9875b8a0abd213b78f9d5ca94bd7c5e9bdea7
Liang Hong <hoilus@users.noreply.github.com>
2018-08-06 16:15:50 -0500

    Update ex11_33-36.cpp

--- Chapter 11-Associative Containers/ex11_33-36.cpp
+++ Chapter 11-Associative Containers/ex11_33-36.cpp
@@ -30,6 +30,20 @@ void word_transform(ifstream &map_file, ifstream &input) {
     }
 }
 
+map<string, string> buildMap(ifstream &map_file) {
+    map<string, string> trans_map;      // holds the transformations
+    string key;                         // a word to transform
+    string value;                       // phrase to use instead
+    // read the first word into key and the rest of the line into value
+    while (map_file >> key && getline(map_file, value))
+        if (value.size() > 1)           // check that there is a transformation
+            trans_map[key] = value.substr(1); // skip leading space
+        else
+            throw runtime_error("no role for " + key);
+    
+    return trans_map;
+}
+
 int main()
 {
     multimap<string, string> author_works = {{"Princeton", "Textbook"}, {"Hong", "Book1"}, {"Hong", "Action2"}, {"Zuo", "Book"}};
